72036a6b630a5420c7efdbf210d61475c98451c3
tman6290 <tofaratifarinu@gmail.com>
2024-03-28 10:06:15 +0000

    I have initialised the project.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet } from 'react-native';
+/*import { StyleSheet } from 'react-native';
 
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
@@ -21,11 +21,39 @@ const styles = StyleSheet.create({
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: 'bold'
+    color: 'white',
   },
   separator: {
     marginVertical: 30,
     height: 1,
     width: '80%',
   },
+});*/
+
+import {View, Text } from 'react-native'
+import { StyleSheet } from 'react-native';
+
+const Home = () => {
+  return(
+    <View>
+      <Text style={styles.title}>Home</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+  
 });
+
+export default Home;
